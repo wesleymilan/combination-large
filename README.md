73b03bab5a967c35d36dc2eea1647d641fc647f1
Wesley Milan <wesleybatista@gmail.com>
2019-09-30 12:32:17 -0300

    stable

--- README.md
+++ README.md
@@ -1,2 +1,141 @@
-# combination-large
-Combine all elements of an array
+# Large Combinator for NodeJS
+
+I built this library to combine big array elements. The others libraries freeze when you try to use arrays larger then 
+30 elements even expanding your nodejs memory `--max-old-space-size=8192`, you **DON'T** need to do this on this library.
+
+## Installing
+`npm i combination-large`
+
+## How to use it
+
+### Basic usage
+All function will returns a result when it's valid and will return `null` and no result can be found.
+
+`next` function is the basic usage, every time you call it the pointer will increment and return the referred combination.
+
+`getLastPosition` function will give you the latest position of the combination pointer, you can use this number to 
+find a specific combination result.
+
+```
+const combination = require('combination-large');
+
+let Combine = new combination({ list: ['a','b','c','d','e'] });
+let combinated, pos;
+
+while(combinated = Combine.next()) {
+    pos = Combine.getLastPosition();
+    console.log(pos + ' - ', combinated);
+}
+```
+
+#### Result
+```
+1 -  [ 'e' ]
+2 -  [ 'd' ]
+3 -  [ 'c' ]
+4 -  [ 'b' ]
+5 -  [ 'a' ]
+6 -  [ 'd', 'e' ]
+7 -  [ 'c', 'e' ]
+8 -  [ 'c', 'd' ]
+9 -  [ 'b', 'e' ]
+10 -  [ 'b', 'd' ]
+11 -  [ 'b', 'c' ]
+12 -  [ 'a', 'e' ]
+13 -  [ 'a', 'd' ]
+14 -  [ 'a', 'c' ]
+15 -  [ 'a', 'b' ]
+16 -  [ 'c', 'd', 'e' ]
+17 -  [ 'b', 'd', 'e' ]
+18 -  [ 'b', 'c', 'e' ]
+19 -  [ 'b', 'c', 'd' ]
+20 -  [ 'a', 'd', 'e' ]
+21 -  [ 'a', 'c', 'e' ]
+22 -  [ 'a', 'c', 'd' ]
+23 -  [ 'a', 'b', 'e' ]
+24 -  [ 'a', 'b', 'd' ]
+25 -  [ 'a', 'b', 'c' ]
+26 -  [ 'b', 'c', 'd', 'e' ]
+27 -  [ 'a', 'c', 'd', 'e' ]
+28 -  [ 'a', 'b', 'd', 'e' ]
+29 -  [ 'a', 'b', 'c', 'e' ]
+30 -  [ 'a', 'b', 'c', 'd' ]
+31 -  [ 'a', 'b', 'c', 'd', 'e' ]
+```
+
+### Previous function
+It will decrement the pointer and return the referred combination.
+```
+combinated = Combine.previous();
+pos = Combine.getLastPosition();
+console.log(pos + ' - ', combinated);
+```
+
+#### Result
+```
+31 -  [ 'a', 'b', 'c', 'd', 'e' ]
+```
+
+### Get Function
+Will return an specific combination based on the position you pass as param.
+```
+combinated = Combine.get(15);
+pos = Combine.getLastPosition();
+console.log(pos + ' - ', combinated);
+```
+
+#### Result
+```
+15 -  [ 'a', 'b' ]
+```
+
+### Range Param
+You can limit the range of elements on a combination passing `range.min` and `range.max` params.
+```
+let Combine = new combination({ list: ['a','b','c','d','e'], range: { min: 3, max: 4 } });
+let combinated, pos;
+
+while(combinated = Combine.next()) {
+    pos = Combine.getLastPosition();
+    console.log(pos + ' - ', combinated);
+}
+```
+
+#### Result
+```
+1 -  [ 'c', 'd', 'e' ]
+2 -  [ 'b', 'd', 'e' ]
+3 -  [ 'b', 'c', 'e' ]
+4 -  [ 'b', 'c', 'd' ]
+5 -  [ 'a', 'd', 'e' ]
+6 -  [ 'a', 'c', 'e' ]
+7 -  [ 'a', 'c', 'd' ]
+8 -  [ 'a', 'b', 'e' ]
+9 -  [ 'a', 'b', 'd' ]
+10 -  [ 'a', 'b', 'c' ]
+11 -  [ 'b', 'c', 'd', 'e' ]
+12 -  [ 'a', 'c', 'd', 'e' ]
+13 -  [ 'a', 'b', 'd', 'e' ]
+14 -  [ 'a', 'b', 'c', 'e' ]
+15 -  [ 'a', 'b', 'c', 'd' ]
+15 -  [ 'a', 'b', 'c', 'd' ]
+15 -  [ 'a', 'b', 'c', 'd' ]
+```
+
+### Reset Function
+You can use reset to start over.
+`Combine.reset();`
+
+This library can be optimized splitting the subgroups reducing the number of cycles to find the result, be my guest
+to fork and send me a pull request.
+
+
+
+
+
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,244 @@
+'use strict';
+
+const debug = require('debug')('combination:large');
+
+/**
+ * Constructor
+ * @param params
+ * @constructor
+ */
+function Combine (params) {
+    this.list = params.list;
+    this.size = params.list.length;
+    this.range = params.range;
+    this.combinations = _numberOfCombinations(this.size, this.range);
+    this.currentPosition = 1;
+    this.targetPosition = 0;
+    this.lastFound = null;
+
+    debug(JSON.stringify(this.combinations, null, 2));
+
+    return;
+}
+
+/**
+ * Get next combination
+ * @returns {*}
+ */
+Combine.prototype.next = function() {
+
+    let self = this;
+
+    self.targetPosition++;
+
+    return self._combine();
+
+};
+
+/**
+ * Get previous combination
+ * @returns {*}
+ */
+Combine.prototype.previous = function() {
+
+    let self = this;
+
+    self.targetPosition--;
+
+    return self._combine();
+
+};
+
+/**
+ * Get latest combination position
+ * @returns {number|*}
+ */
+Combine.prototype.getLastPosition = function() {
+    return this.targetPosition;
+};
+
+/**
+ * Get an specific combination base on it's position
+ * @param position
+ * @returns {*}
+ */
+Combine.prototype.get = function(position) {
+
+    let self = this;
+
+    self.targetPosition = position;
+
+    return self._combine();
+
+};
+
+Combine.prototype.reset = function() {
+    this.targetPosition = 0;
+};
+
+/**
+ * Private Method: Prepare combination process
+ * @returns {null|*}
+ * @private
+ */
+Combine.prototype._combine = function() {
+
+    let self = this;
+
+    self.lastFound = null;
+
+    let position = self._getPositions(self.targetPosition);
+
+    if(!position) return null;
+
+    self.currentPosition = position.start -1;
+
+    let blankSpots = self.size - position.pos;
+    let fill = position.pos;
+
+    let result = self._combineSequence([], 0, blankSpots, fill, position);
+
+    result = result.map(function(i) {
+        if(self.list[i]) return self.list[i];
+    });
+
+    result = result.filter(function(i) {
+        if(typeof i !== 'undefined' && i !== null) return i;
+    });
+
+    return result;
+
+};
+
+/**
+ * Private Method: Run combination
+ * @param result
+ * @param currentLevel
+ * @param blankSpots
+ * @param fill
+ * @param position
+ * @returns {*}
+ * @private
+ */
+Combine.prototype._combineSequence = function(result, currentLevel, blankSpots, fill, position) {
+
+    let self = this;
+
+    for(let i = 0; i < 2; i++) {
+
+        if(self.currentPosition === self.targetPosition)return result;
+
+        debug('currentLevel:', currentLevel);
+        debug('blankSpots: ', blankSpots);
+        debug('fill: ', fill);
+        debug('position: ', JSON.stringify(position, null, 2));
+        debug('currentPosition', self.currentPosition);
+        debug('targetPosition', self.targetPosition);
+
+        result.splice(currentLevel, self.size-currentLevel);
+
+        debug('i: ', i);
+
+        if(i === 0 && blankSpots > 0) {
+
+            result[currentLevel] = null;
+            debug('result 1: ', result);
+            if (currentLevel + 1 < self.size) result = self._combineSequence(result, currentLevel + 1, blankSpots - 1, fill, position);
+
+        } else if(fill > 0) {
+
+            result[currentLevel] = currentLevel;
+            debug('result 3: ', result);
+            if(currentLevel + 1 < self.size) result = self._combineSequence(result, currentLevel + 1, blankSpots, fill - 1, position);
+
+        }
+
+        let currentFound = result.join('-');
+
+        if(currentLevel + 1 === self.size && currentFound !== self.lastFound) {
+            self.lastFound = currentFound;
+            self.currentPosition++;
+            return result;
+        }
+
+    }
+
+    return result;
+
+};
+
+/**
+ * Private Method: Get position list
+ * @param position
+ * @returns {null|*}
+ * @private
+ */
+Combine.prototype._getPositions = function(position) {
+
+    if(this.combinations.total < position) return null;
+
+    for(let i in this.combinations.list) {
+        if(position >= this.combinations.list[i].start && position <= this.combinations.list[i].end) {
+            return this.combinations.list[i];
+        }
+    }
+
+    return null;
+
+};
+
+/**
+ * Private Method: Get Combination Map strategy
+ * @param size
+ * @param range
+ * @returns {{total: number, list: {}}}
+ * @private
+ */
+function _numberOfCombinations(size, range) {
+
+    if(!range) {
+        range = {
+            min: 1,
+            max: size
+        }
+    } else {
+        if(!range.min) {
+            range.min = 1;
+        }
+        if(!range.max) {
+            range.max = size;
+        }
+    }
+
+    let result = {
+        total: 0,
+        list: {}
+    };
+
+    for(let i = range.min; i <= range.max; i++) {
+        result.list[i] = {};
+        result.list[i].pos = i;
+        result.list[i].qty = _fac(size) / (_fac(i) * _fac(size - i));
+        result.list[i].start = result.list[i-1] ? result.list[i-1].end + 1 : 1;
+        result.list[i].end = result.list[i].start + result.list[i].qty - 1;
+        result.total += result.list[i].qty;
+    }
+
+    return result;
+
+}
+
+/**
+ * Private Method: Factorial function
+ * @param n
+ * @returns {number}
+ * @private
+ */
+function _fac(n){
+    return(n<2)?1:_fac(n-1)*n;
+}
+
+module.exports = Combine;
+
+
+
